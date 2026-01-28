04fb583cf16cc7231dec4ff9badf6f04346dc72f
Energy burner of motivation <prakashbabuparamhans729srmt@gmail.com>
2026-01-28 19:16:54 +0000

    logo dikh nahi raha hai

--- src/components/icons/AishaLogo.tsx
+++ src/components/icons/AishaLogo.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image';
 import * as React from 'react';
+import Image from 'next/image';
 
 interface AishaLogoProps {
   className?: string;
@@ -7,19 +7,15 @@ interface AishaLogoProps {
   height: number;
 }
 
-// डेवलपर नोट: अपना कस्टम लोगो इस्तेमाल करने के लिए, कृपया यह करें:
-// 1. अपनी लोगो फ़ाइल (जैसे, 'aisha-logo.svg') को प्रोजेक्ट के रूट में 'public' डायरेक्टरी के अंदर रखें।
-// 2. यदि आपने अपनी फ़ाइल का नाम बदला है, तो नीचे 'src' विशेषता को '/aisha-logo.svg' से बदलकर '/your-file-name.svg' कर दें।
-
+// This component loads the logo from /public/aisha-logo.svg
 export const AishaLogo = (props: AishaLogoProps) => {
   return (
     <Image
-      src="/aisha-logo.svg" 
+      src="/aisha-logo.svg"
       alt="Aisha Logo"
       width={props.width}
       height={props.height}
       className={props.className}
-      unoptimized={true}
     />
   );
 };
